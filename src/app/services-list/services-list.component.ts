eef60f3acc57e2fe54c4cb1ee6ccfbc2cb4eaed2
marcsalou23 <127138098+marcsalou23@users.noreply.github.com>
2024-02-27 12:31:30 +0100

    forma imagenes

--- src/app/services-list/services-list.component.ts
+++ src/app/services-list/services-list.component.ts
@@ -11,21 +11,21 @@ export class ServicesListComponent implements OnInit {
       {
         name: 'Transporte y exquisita cata de vinos en el Priorat (ida y vuelta)',
         price: '50€ por persona',
-        image: 'wine.jpg',
+        image: '../../assets/images/wine.jpg',
         description:
           'Disfrute de un día de transporte privado y degustación de vinos en el Priorat.',
       },
       {
         name: 'Transporte a Montserrat (ida y vuelta)',
         price: '40€ por persona',
-        image: 'montserrat.jpg',
+        image: '../../assets/images/montserrat.jpg',
         description:
           'Excursión de día completo a la impresionante montaña de Montserrat.',
       },
       {
         name: 'Transporte a Barcelona (ida y vuelta)',
         price: '50€ por persona',
-        image: 'barcelona.jpg',
+        image: '../../assets/images/barcelona.jpg',
         description: 'Viaje de ida y vuelta a la vibrante ciudad de Barcelona.',
       },
     ],
@@ -33,19 +33,19 @@ export class ServicesListComponent implements OnInit {
       {
         name: 'Transporte a Salou (ida y vuelta)',
         price: '25€ por persona',
-        image: 'salou.jpg',
+        image: '../../assets/images/salou.jpg',
         description: 'Traslado de ida y vuelta a las hermosas playas de Salou.',
       },
       {
-        name: 'Visita a Tarragona (ida y vuelta)',
+        name: 'Visita Tarragona (ida y vuelta)',
         price: '30€ por persona',
-        image: 'tarragona.jpg',
+        image: '../../assets/images/tarragona.jpg',
         description: 'Descubre la historia y la belleza de Tarragona.',
       },
       {
         name: 'Excursión guiada por las montañas de Prades (ida y vuelta)',
         price: '50€ por persona',
-        image: 'prades.jpg',
+        image: '../../assets/images/prades.jpg',
         description:
           'Disfruta de la naturaleza y la aventura en las montañas de Prades.',
       },
@@ -53,8 +53,8 @@ export class ServicesListComponent implements OnInit {
     [
       {
         name: 'Personaliza tu trip!',
-        price: 'Varía',
-        image: 'custom.jpg',
+        price: 'Variable',
+        image: '../../assets/images/custom.jpg',
         description:
           'Dinos donde quieres ir y te haremos un presupuesto personalizado.',
       },
