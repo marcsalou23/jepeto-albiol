f3d4af5826fa9a53e69d49214311717e561cb234
marcsalou23 <127138098+marcsalou23@users.noreply.github.com>
2024-03-04 01:13:21 +0100

    revert

---
+++ src/app/views/app-airbnb.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-airbnb',
+  templateUrl: './app-airbnb.component.html',
+})
+export class AppAirbnbComponent {
+  localBooking: boolean = false;
+}
