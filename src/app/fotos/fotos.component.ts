420667447a35f953e4e9bcec7aa72e3812cf0246
marcsalou23 <127138098+marcsalou23@users.noreply.github.com>
2024-04-11 01:42:43 +0200

    Optimize Image Loading with Lazy Loading Implementation
    This commit introduces a significant performance optimization to the website's gallery feature by implementing lazy loading for images. Previously, all images on the gallery page were loaded simultaneously, regardless of whether they were in the viewport. This behavior contributed to increased page load times and a suboptimal user experience, especially on pages with a large number of images.
    
    Changes made include:
    - The `FotosComponent` has been updated to contain an array of photo objects, each comprising the image source (`src`) and alternative text (`alt`). This allows for dynamic rendering of images in the template.
    - The gallery HTML template (`fotos.component.html`) has been modified to utilize Angular's `*ngFor` directive for iterating over the photo objects array. Additionally, the `loading="lazy"` attribute has been added to all `<img>` tags to enable browser-native lazy loading of images.
    - CSS and responsive design adjustments have been retained, ensuring that the user interface remains visually appealing and functional across different device sizes.
    
    This lazy loading implementation leverages the latest web standards, improving the site's overall performance by reducing initial load time and conserving bandwidth. It is particularly beneficial for users on slower internet connections or devices with limited resources.
    
    By deferring the loading of off-screen images until they are about to enter the viewport, the website now enjoys enhanced responsiveness and a smoother scrolling experience. This update aligns with best practices for modern web development and significantly contributes to improved site metrics, such as the Speed Index and Time to Interactive.

--- src/app/fotos/fotos.component.ts
+++ src/app/fotos/fotos.component.ts
@@ -6,6 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./fotos.component.css'],
 })
 export class FotosComponent {
-  // Define the photoNumbers array
-  photoNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  photos = [
+    { src: '../../assets/images/IMG-20240228-WA0069.jpg', alt: 'Photo 1' },
+    { src: '../../assets/images/IMG-20240228-WA0133.jpg', alt: 'Photo 2' },
+    { src: '../../assets/images/IMG20200302164944.jpg', alt: 'Photo 3' },
+    { src: '../../assets/images/IMG20200302164721.jpg', alt: 'Photo 4' },
+    { src: '../../assets/images/IMG20200302164540.jpg', alt: 'Photo 5' },
+    { src: '../../assets/images/IMG20200302164515.jpg', alt: 'Photo 6' },
+    { src: '../../assets/images/IMG20200302172748.jpg', alt: 'Photo 7' },
+    { src: '../../assets/images/IMG20200302172739.jpg', alt: 'Photo 8' },
+    { src: '../../assets/images/IMG20200302172731.jpg', alt: 'Photo 9' },
+    { src: '../../assets/images/IMG20200302170422.jpg', alt: 'Photo 10' },
+    { src: '../../assets/images/IMG20200302170409.jpg', alt: 'Photo 11' },
+    { src: '../../assets/images/IMG20200302170343.jpg', alt: 'Photo 12' },
+    { src: '../../assets/images/IMG20200302170229.jpg', alt: 'Photo 13' },
+    { src: '../../assets/images/IMG20200302170149.jpg', alt: 'Photo 14' },
+    { src: '../../assets/images/IMG20200302170138.jpg', alt: 'Photo 15' },
+    { src: '../../assets/images/IMG20200302170126.jpg', alt: 'Photo 16' },
+    { src: '../../assets/images/IMG20200302170111.jpg', alt: 'Photo 17' },
+    { src: '../../assets/images/IMG20200302170057.jpg', alt: 'Photo 18' },
+    { src: '../../assets/images/IMG20200302165915.jpg', alt: 'Photo 19' },
+    { src: '../../assets/images/IMG20200302165830.jpg', alt: 'Photo 20' },
+    { src: '../../assets/images/IMG20200302165655.jpg', alt: 'Photo 21' },
+    { src: '../../assets/images/IMG20200302165633.jpg', alt: 'Photo 22' },
+    { src: '../../assets/images/IMG20200302165625.jpg', alt: 'Photo 23' },
+    { src: '../../assets/images/IMG20200302165529.jpg', alt: 'Photo 24' },
+    { src: '../../assets/images/IMG20200302165526.jpg', alt: 'Photo 25' },
+    { src: '../../assets/images/IMG20200302165431.jpg', alt: 'Photo 26' },
+    { src: '../../assets/images/IMG20200302165421.jpg', alt: 'Photo 27' },
+    { src: '../../assets/images/IMG20200302165416.jpg', alt: 'Photo 28' },
+    { src: '../../assets/images/IMG20200302165358.jpg', alt: 'Photo 29' },
+    { src: '../../assets/images/IMG20200302165350.jpg', alt: 'Photo 30' },
+    { src: '../../assets/images/coffe.jpg', alt: 'Coffee Photo' },
+  ];
 }
